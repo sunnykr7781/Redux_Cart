4a4536f3a5cf291358be5c5e8c2a6ae13a138cba
sunny <sunny129271@gmail.com>
2024-04-16 03:14:14 +0530

    added state management using redux

---
+++ src/components/Home.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+  "https://images.unsplash.com/photo-1491553895911-0055eca6402d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+const Home = () => {
+  const productList = [
+    {
+      name: "Mac Book",
+      price: 12000,
+      imgSrc: img1,
+      id: "asdhajsdbhjabhsjdfdfv",
+    },
+    {
+      name: "Black Shoes",
+      price: 490,
+      imgSrc: img2,
+      id: "sdjfdlaajsdbhjabhsjdfdfv",
+    },
+  ];
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (options) => {
+    dispatch({ type: "addToCart", payload: options });
+    dispatch({ type: "calculatePrice" });
+    toast.success("Added To Cart");
+  };
+  return (
+    <div className="home">
+      {productList.map((i) => (
+        <ProductCard
+          key={i.id}
+          imgSrc={i.imgSrc}
+          name={i.name}
+          price={i.price}
+          id={i.id}
+          handler={addToCartHandler}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export default Home;
